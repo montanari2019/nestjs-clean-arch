ffb2828d98b3fcf076c2b376ccfe6b10176d00c7
montanari2019 <ikaro.montanari31@gmail.com>
2025-02-03 16:17:35 -0400

    getters test unit methods

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -21,4 +21,24 @@ describe('UserEntity unit test', () => {
     expect(systemUnderTest.props.password).toEqual(props.password);
     expect(systemUnderTest.props.created_at).toBeInstanceOf(Date);
   });
+
+  it('Name getter of filed method', () => {
+    expect(systemUnderTest.props.name).toBeDefined();
+    expect(systemUnderTest.props.name).toEqual(props.name);
+    expect(typeof systemUnderTest.props.name).toBe('string');
+  });
+  it('Email getter of filed method', () => {
+    expect(systemUnderTest.props.email).toBeDefined();
+    expect(systemUnderTest.props.email).toEqual(props.email);
+    expect(typeof systemUnderTest.props.email).toBe('string');
+  });
+  it('PassWord getter of filed method', () => {
+    expect(systemUnderTest.props.password).toBeDefined();
+    expect(systemUnderTest.props.password).toEqual(props.password);
+    expect(typeof systemUnderTest.props.password).toBe('string');
+  });
+  it('Created At getter of filed method', () => {
+    expect(systemUnderTest.props.created_at).toBeDefined();
+    expect(systemUnderTest.props.created_at).toBeInstanceOf(Date);
+  });
 });
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,17 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.created_at = this.props.created_at ?? new Date();
   }
+
+  get name(): string {
+    return this.props.name;
+  }
+  get email(): string {
+    return this.props.email;
+  }
+  get password(): string {
+    return this.props.password;
+  }
+  get createdAt(): Date | undefined {
+    return this.props.created_at;
+  }
 }
